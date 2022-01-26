9284283b38cab54ab53f80b7ac03f92d35626655
joyfun <joyfun@joyfun.net>
2022-01-26 09:40:24 +0800

    add pcbeta

--- scripts/pcbeta.js
+++ scripts/pcbeta.js
@@ -2,7 +2,7 @@ const axios = require("axios");
 function pcbeta() {
   return new Promise(async (resolve) => {
     try {
-      let url = "http://i.pcbeta.com/home.php?mod=task&do=draw&id=149";
+      let url = "http://i.pcbeta.com/home.php?mod=task&do=apply&id=149";
       const header = {
         headers: {
           Referer: "https://i.pcbeta.com/home.php?mod=task",
