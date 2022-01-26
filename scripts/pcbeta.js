b8a8128aef336232e35c9dd651f6d4678ed2e4fd
joyfun <joyfun@joyfun.net>
2022-01-26 09:28:07 +0800

    add pcbeta

---
+++ scripts/pcbeta.js
@@ -0,0 +1,27 @@
+const axios = require("axios");
+function pcbeta() {
+  return new Promise(async (resolve) => {
+    try {
+      let url = "http://i.pcbeta.com/home.php?mod=task&do=draw&id=149";
+      const header = {
+        headers: {
+          Referer: "https://i.pcbeta.com/home.php?mod=task",
+          cookie: config.pcbeta.cookie,
+        },
+      };
+      let res = await axios.get(url, header);
+      if (res.data) {
+        data = res.data
+      } else {
+        data = res.data.error_msg;
+      }
+      console.log(data);
+    } catch (err) {
+      console.log(err);
+      data="签到接口请求出错"
+    }
+    resolve("【远景论坛】：" + data);
+  });
+}
+
+module.exports = pcbeta;
